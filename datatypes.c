95d7b9d5fe366cf5ac4097d755f49c56d1aae08c
Priyanshu-code227 <priyanshumishra232@lpu.in>
2024-03-18 01:47:13 +0530

    Datatypes in C

--- datatypes.c
+++ datatypes.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(){
     int a = 15; //this is integer type
-    printf("%d",a); //we use %d to  print  integer
+    printf("%d",a); //we use %d to  print  integer it  will print 15
 
     float b = 12.0; //this is float (decimal) type
-    printf("%f",b);//we use %f to print float 
+    printf("%f",b);//we use %f to print float it will print 12.000000
 
     char c= "Priyanshu"; //this is character type in double quoation not  single quotation
-    printf("%c",c);// we use %c  to print  character
-    
+    printf("%c",c);// we use %c  to print  character it will print Priyanshu
+
     double d = 15.2; // this  is double in this  we can use both float and integer
-    printf("%lf",d); //we use %lf to print double 
+    printf("%lf",d); //we use %lf to print double it will print 15.200000
 }
