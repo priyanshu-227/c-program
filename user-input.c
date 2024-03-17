c5e3312a4ddf378f36e58b909154fa96701abacd
Priyanshu-code227 <priyanshumishra232@lpu.in>
2024-03-18 01:44:02 +0530

    User  input

---
+++ user-input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a;//we need to define the datatype for the varibale beforewe can input it's value
+    scanf("%d",&a);//scanf is used take input from user &a means that we are taking input for 'a' %d says that datatype fora is integer
+
+    float b;
+    scanf("%f",&b); //datatype is float and we are taking input for 'b'
+
+}
