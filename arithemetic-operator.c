1c5d62ad13d783c78ed9709697869029b0b672af
Priyanshu-code227 <priyanshumishra232@lpu.in>
2024-03-18 01:59:10 +0530

    Arithematic Operator in C

---
+++ arithemetic-operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a,b; //it will define two integers a and b
+    scanf("%d %d",&a,&b);// it will input both a and b
+    int c =  a+b;//it will  add 'a' and 'b' and store the sum in any variable c
+    printf("%d",c);//it will print c
+
+    
+}
